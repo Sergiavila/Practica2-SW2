4adf25a5a39784660f3c719bee58212f6096e889
Carlos Palomo Rey <c.palomo1@usp.ceu.es>
2021-04-20 18:01:21 +0200

    WEB-INF

--- Pr2_SWII/src/Pr2_SWII/soap/Methods.java
+++ Pr2_SWII/src/Pr2_SWII/soap/Methods.java
@@ -1,5 +1,24 @@
 package Pr2_SWII.soap;
 
+import javax.jws.WebMethod;
+
+import pojos.Recetario;
+
 public class Methods {
 
+	@WebMethod(operationName = "hello")
+	public String crearRecetario() {
+	        Recetario recetario = new Recetario();
+	        
+	        //A quitar
+	        return "Hello";
+	}
+	
+	/*
+	@WebMethod(operationName = "hello")
+	public String hello(@WebParam(name = "name") Receta receta) {
+	        return "Hello " + txt + " !";
+	}
+	*/
+	
 }
